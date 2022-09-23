c89036fcb731badc37ac1d083c3fa71ed82c9314
vishnuwe45 <87526447+vishnuwe45@users.noreply.github.com>
2022-09-23 15:31:46 +0530

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -2,29 +2,6 @@ from gaiasdk import sdk
 import logging
 import time
 
-
-# def RunBandit(args):
-#     logging.info("Bandit has been started!")
-#     time.sleep(5)
-#     logging.info("Bandit has been finished!")
-
-# def RunSafety(args):
-#     logging.info("Safety has been started!")
-#     time.sleep(5)
-#     logging.info("Safety has been finished!")
-
-# def RunPyraider(args):
-#     logging.info("Pyraider has been started!")
-#     time.sleep(5)
-#     logging.info("Pyraider has been finished!")
-
-# def main():
-#     logging.basicConfig(level=logging.INFO)
-#     runbandit = sdk.Job("Run Bandit Scan", "Running Bandit Scan", RunBandit, ["Run Bandit Scan"])
-#     runsafety = sdk.Job("Run Safety Scan", "Running Safety Scan", RunSafety, ["Run Bandit Scan"])
-#     runpyraider = sdk.Job("Run Pyraider Scan", "Running Pyraider Scan", RunPyraider, ["Run Safety Scan"])
-#     sdk.serve([runbandit, runsafety, runpyraider])
-
 def CreateUser(args):
     logging.info("CreateUser has been started!")
     time.sleep(5)
